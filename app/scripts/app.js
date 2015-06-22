6dde453e52355592ccf10779b7dc97baa70623d5
karimhmaissi <karimhmaissi@gmail.com>
2015-06-22 14:50:30 +0100

    init repo

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name collectFrontEndApp
+ * @description
+ * # collectFrontEndApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('collectFrontEndApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
